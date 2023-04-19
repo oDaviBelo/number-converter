3919d9d5c7ec1fde5a1bae9c58eeb109565462b1
Davi Belo <davibelocontato@gmail.com>
2023-04-19 16:06:32 -0300

    bintodec

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ let button = document.querySelector('#button');
 let input = document.querySelector('.input');
 let resul = document.querySelector(".binres");
 let opt = document.querySelector('select');
-button.addEventListener('click', () => {
-    input.innerHTML = '';
-    converter();
-});
 function converter() {
     let dec = +input.value;
     if ((opt === null || opt === void 0 ? void 0 : opt.value) === 'dtob') {
@@ -42,4 +38,44 @@ function converter() {
             resul.innerHTML += ` ${trans}`;
         }
     }
+    else if ((opt === null || opt === void 0 ? void 0 : opt.value) === 'btod') {
+        let num = input.value;
+        let arrSort = Array.from(num);
+        arrSort.map((x) => {
+            if (x === '2' || x === '3' || x === '4' || x === '5' || x === '6' || x === '7' || x === '8' || x === '9') {
+                resul.innerHTML = `Digite Somente 0 e 1`;
+            }
+            else {
+                if (isNaN(+num)) {
+                    input.value = '';
+                    alert(`Digite apenas (0,1)`);
+                }
+                else {
+                    bin();
+                }
+            }
+        });
+        function bin() {
+            var arr = arrSort.slice(0).reverse();
+            let newArr = [];
+            let sum = 0;
+            for (let i = 0; i <= arr.length; i++) {
+                let newNum = +arr[i];
+                let pow = Math.pow(2, i);
+                if (isNaN(newNum)) {
+                }
+                else {
+                    resul.innerHTML = '';
+                    let calc = newNum * pow;
+                    newArr[i] = calc;
+                    sum += newArr[i];
+                    resul.innerHTML += `${sum}`;
+                }
+            }
+        }
+    }
 }
+button.addEventListener('click', () => {
+    input.innerHTML = '';
+    converter();
+});
--- script.ts
+++ script.ts
@@ -3,10 +3,6 @@ let input = document.querySelector('.input') as HTMLInputElement;
 let resul = document.querySelector(".binres") as any;
 let opt = document.querySelector('select');
 
-button.addEventListener('click', ()=>{
-    input.innerHTML = ''
-    converter()
-});
 
 function converter(){
     let dec = +input.value;
@@ -39,5 +35,51 @@ function converter(){
             resul.innerHTML += ` ${trans}`; 
         }
     }
-    
+    else if(opt?.value === 'btod'){
+
+        let num = input.value;
+        let arrSort = Array.from(num);
+
+        arrSort.map((x)=>{
+            if(x === '2' ||x === '3' ||x === '4' ||x === '5' ||x === '6' ||x === '7' ||x === '8' ||x === '9'){
+                resul.innerHTML = `Digite Somente 0 e 1`
+
+            }
+            else{
+                if(isNaN(+num)){
+                    input.value = ''
+                    alert(`Digite apenas (0,1)`)
+                }else{  
+                    bin()
+                }
+            }
+        })
+       
+        function bin(){
+            
+            var arr = arrSort.slice(0).reverse();
+            let newArr = [];
+            let sum = 0;
+            for(let i = 0; i <= arr.length; i++){ 
+                let newNum = +arr[i];
+                let pow = Math.pow(2,i);
+                if(isNaN(newNum)){
+        
+                }else{
+                resul.innerHTML = '';
+                    let calc = newNum*pow;
+                    newArr[i] = calc
+                    sum += newArr[i];   
+                    resul.innerHTML += `${sum}`
+                }
+            }
+        }
+    }
 }
+
+button.addEventListener('click', ()=>{
+    input.innerHTML = ''
+    converter()
+});
+
+
